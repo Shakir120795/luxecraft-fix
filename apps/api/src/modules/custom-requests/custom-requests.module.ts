8cae0177fa21c727a067291ee34db3af74296c4e
shakir malik <154538573+Shakir120795@users.noreply.github.com>
2026-09-10 02:47:21 +0530

    feat: wire uploads into custom requests

--- apps/api/src/modules/custom-requests/custom-requests.module.ts
+++ apps/api/src/modules/custom-requests/custom-requests.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CustomRequestsService } from './custom-requests.service';
 import { CustomRequestsController } from './custom-requests.controller';
 import { CustomMessagesModule } from '../custom-messages/custom-messages.module';
+import { UploadsModule } from '../uploads/uploads.module';
 
 @Module({
-  imports: [CustomMessagesModule],
+  imports: [CustomMessagesModule, UploadsModule],
   controllers: [CustomRequestsController],
   providers: [CustomRequestsService],
   exports: [CustomRequestsService],
