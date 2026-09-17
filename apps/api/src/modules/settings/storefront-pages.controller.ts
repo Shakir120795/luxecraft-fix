9f14407c03af25b039a2300598b2b6f5f944e072
shakir malik <154538573+Shakir120795@users.noreply.github.com>
2026-09-17 12:54:23 +0530

    feat: add public storefront site pages endpoint

---
+++ apps/api/src/modules/settings/storefront-pages.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { SettingsService } from './settings.service';
+
+@Controller('storefront/pages')
+export class StorefrontPagesController {
+  constructor(private readonly settings: SettingsService) {}
+
+  @Get()
+  getPages() {
+    return this.settings.getSitePages();
+  }
+
+  @Get(':slug')
+  getPage(@Param('slug') slug: string) {
+    return this.settings.getSitePage(slug);
+  }
+}
